19649aefb7fc3401e2a043a3bd6d14fe855c3da0
Blarc <jakob.malezic@gmail.com>
2021-05-22 19:27:58 +0200

    ForgotPasswordPage & ForgotPasswordEmailSentPage: added basic forgot password flow

--- src/app/pages/public/auth/forgot-password/forgot-password.page.ts
+++ src/app/pages/public/auth/forgot-password/forgot-password.page.ts
@@ -1,11 +1,20 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   templateUrl: './forgot-password.page.html',
   styleUrls: ['./forgot-password.page.scss'],
 })
 export class ForgotPasswordPage implements OnInit {
-  constructor() {}
+
+  constructor(
+    private router: Router
+  ) {
+  }
 
   ngOnInit(): void {}
+
+  onClickResetPassword() {
+    this.router.navigate(["/forgot-password-email-sent"]);
+  }
 }

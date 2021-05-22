8016f981505c30ae70518a75e099e0f8c2e4a639
Bregzl <matic.bregar98@gmail.com>
2021-05-22 22:55:30 +0200

    japop

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { LeafletModule } from '@asymmetrik/ngx-leaflet';
 import { CoreModule } from './@core/core.module';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LayoutModule } from './components/layout/layout.module';
-
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, CoreModule, LayoutModule],
+  imports: [BrowserModule, AppRoutingModule, CoreModule, LayoutModule, LeafletModule],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
